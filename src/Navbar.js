219b8e2ff7ca992eae6fe4060c91106f62fc9bc8
joshrs1199 <46760607+joshrs1199@users.noreply.github.com>
2019-07-28 23:59:59 -0400

    created contact page and made posts to backend on submit

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = (props) => {
       <Menu.Item href='/contact'>Contact</Menu.Item>
       {props.currentUser ? <Menu.Item onClick={props.logOut}>Logout</Menu.Item> : <Menu.Item href='/login'>Login / Signup</Menu.Item>}
       {props.currentUser ? <Menu.Item href='/shoppingcart'>ShoppingCart</Menu.Item> : null}
-      {props.currentUser ? <Menu.Item href='/profile'>Profile</Menu.Item> : null}
+      {/* {props.currentUser ? <Menu.Item href='/profile'>Profile</Menu.Item> : null} */}
     </Menu>
   )
 }
